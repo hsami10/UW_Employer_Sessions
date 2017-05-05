bd87081c865d98b9bb58a33404391eb5619c5081
Hassaan Sami <hassaan.sami@live.com>
2017-05-04 22:16:35 -0400

    Create function for replacing placeholder text in templates.

--- app/server/renderer.js
+++ app/server/renderer.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 
+const insertValues = () => {
+
+};
+
 const display = (templateName, values, response) => {
     //read from the template file
     let fileContents = fs.readFileSync(`../html_templates/${templateName}.html`, {encoding: "utf8"});
     //Replace tagged values (with {{}}) with 'values'
+    fileContents = insertValues();
     //Write new html contents to response
 };
