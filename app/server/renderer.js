128e5178bdfc01d32cce998008517c8ad2ed916b
Hassaan Sami <hassaan.sami@live.com>
2017-05-04 22:20:10 -0400

    Complete renderer.js file: Read file, insert values, and write to response.

--- app/server/renderer.js
+++ app/server/renderer.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
-const insertValues = () => {
-
+const insertValues = (fileContents, values) => {
+    //loop through values properties and populate fileContents with the values in it.
+    for (const key in values) {
+        fileContents = fileContents.replace(`{{${key}}}`, values[key]);
+    }
+    return fileContents;
 };
 
 const display = (templateName, values, response) => {
     //read from the template file
     let fileContents = fs.readFileSync(`../html_templates/${templateName}.html`, {encoding: "utf8"});
     //Replace tagged values (with {{}}) with 'values'
-    fileContents = insertValues();
+    fileContents = insertValues(fileContents, values);
     //Write new html contents to response
+    response.write(fileContents);
 };
+
+module.exports.display = display;
