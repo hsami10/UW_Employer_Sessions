45fe6e79a5c20e94cb16e0141e9d5450ca9d24a3
Hassaan Sami <hassaan.sami@live.com>
2017-05-04 22:15:09 -0400

    Read file contents from respective html template, in renderer.js

--- app/server/renderer.js
+++ app/server/renderer.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
 const display = (templateName, values, response) => {
-
+    //read from the template file
+    let fileContents = fs.readFileSync(`../html_templates/${templateName}.html`, {encoding: "utf8"});
+    //Replace tagged values (with {{}}) with 'values'
+    //Write new html contents to response
 };
