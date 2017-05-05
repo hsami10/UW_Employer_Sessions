1fdd270b4318d2b26c5d35cb015312cffb077776
Hassaan Sami <hassaan.sami@live.com>
2017-05-05 12:35:18 -0400

    Abstract away the data fetching of relevent data from json and rendering into another function.

--- app/server/router.js
+++ app/server/router.js
@@ -4,6 +4,32 @@ const uwaterlooApi = require('uwaterloo-api');
 const uwApi = require('./uw_api.json');
 const renderer = require('./renderer.js');
 
+//helper function to extract data from json and embed them into an object, then render them with renderer
+const extractData = (session) => {
+    //construct the query string for the clearbit search for company logo
+    const logoQuery = session.employer.replace(' ', '').toLowerCase();
+    //construct a string with <li> tags, each for a target audience
+    let audience = ``;
+    session.audience.forEach((val) => {
+        audience += `<li>${val}</li>`;
+    });
+
+    const values = {
+        logoQuery: logoQuery,
+        googleLocation: "https://screenshots.en.sftcdn.net/en/scrn/97000/97769/google-maps-53-535x535.png",
+        employerName: session.employer,
+        day: session.day,
+        date: session.date,
+        startTime: session.start_time,
+        endTime: session.end_time,
+        buildingUrl: session.building.map_url,
+        buildingNRoom: `${session.building.code} ${session.building.room}`,
+        audienceList: audience,
+        description: session.description
+    };
+    renderer.display("info_session", values, response);
+}
+
 const home = (request, response) => {
     //Handle HTTP GET request to home page
     if (request.url === '/') {
@@ -24,31 +50,9 @@ const home = (request, response) => {
                 data.forEach((session, index) => {
                     //if its a closed information session, skip over this one
                     const checkClosed = session.employer.toLowerCase();
-                    if (checkClosed.includes("Closed Info Session") || checkClosed.includes("Closed Information Session")) {
+                    if (checkClosed.includes("closed info session") || checkClosed.includes("closed information session")) {
                         return;
                     }
-                    //construct the query string for the clearbit search for company logo
-                    const logoQuery = session.employer.replace(' ', '').toLowerCase();
-                    //construct a string with <li> tags, each for a target audience
-                    let audience = ``;
-                    session.audience.forEach( (val) => {
-                        audience += `<li>${val}</li>`;
-                    });
-                    
-                    const values = {
-                        logoQuery: logoQuery,
-                        googleLocation: "https://screenshots.en.sftcdn.net/en/scrn/97000/97769/google-maps-53-535x535.png",
-                        employerName: session.employer,
-                        day: session.day,
-                        date: session.date,
-                        startTime: session.start_time,
-                        endTime: session.end_time,
-                        buildingUrl: session.building.map_url,
-                        buildingNRoom: `${session.building.code} ${session.building.room}`,
-                        audienceList: audience,
-                        description: session.description
-                    };
-                    renderer.display("info_session", values, response);
                 });
 
                 renderer.display("footer", {}, response);
